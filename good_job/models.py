432e9916d7ae1e28b72e66a9f18adb7b01971154
okhremenko-vladimir <vladimir.okhremenko@gmail.com>
2022-08-16 00:21:47 +0700

    template editing

--- good_job/models.py
+++ good_job/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    company_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32)
+    location = models.CharField(max_length=32)
+    logo = models.URLField(default='https://place-hold.it/100x60')
+    description = models.TextField()
+    employee_count = models.IntegerField()
+
+
+class Specialty(models.Model):
+    code = models.CharField(max_length=32)
+    title = models.CharField(max_length=32)
+    picture = models.URLField(default='https://place-hold.it/100x60')
+
+
+class Vacancy(models.Model):
+    vacancy_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=32)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE, related_name="vacancies")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="vacancies")
+    skills = models.CharField(max_length=128)
+    description = models.TextField()
+    salary_min = models.IntegerField()
+    salary_max = models.IntegerField()
+    published_at = models.DateField()
+
+    def skills_through_point(self):
+        return ' • '.join(self.skills.split(', '))
--- good_job/views.py
+++ good_job/views.py
@@ -1,27 +1,45 @@
+from django.http import HttpResponseNotFound, HttpResponseServerError
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Vacancy, Company, Specialty
 
 
 def main_view(request):
-    context = {}
+    specialties = Specialty.objects.all()
+    vacancies = Vacancy.objects.all()
+    companies = Company.objects.all()
+    context = {'specialties': specialties, 'vacancies': vacancies, 'companies': companies}
     return render(request, 'good_job/index.html', context)
 
 
 def vacancies_view(request):
-    context = {}
+    vacancies = Vacancy.objects.all()
+    context = {'vacancies': vacancies}
     return render(request, 'good_job/vacancies.html', context)
 
 
 def vacancies_cat_view(request, speciality):
-    context = {}
+    vacancies_by_speciality = Vacancy.objects.filter(specialty__code=speciality)
+    context = {'speciality': speciality, 'vacancies_by_speciality': vacancies_by_speciality}
     return render(request, 'good_job/vacancies_cat.html', context)
 
 
 def company_view(request, company_id):
-    context = {}
+    company = Company.objects.get(company_id=company_id)
+    vacancies = Vacancy.objects.filter(company=company_id)
+    context = {'company': company, 'vacancies': vacancies}
     return render(request, 'good_job/company.html', context)
 
 
 def vacancy_view(request, vacancy_id):
-    context = {}
+    vacancy = Vacancy.objects.get(vacancy_id=vacancy_id)
+    context = {'vacancy': vacancy}
     return render(request, 'good_job/vacancy.html', context)
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Ресурс не найден!')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('Ошибка сервера!')
