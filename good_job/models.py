6c4f8aaa38827f841b64ce148ed5d74f87aaf222
okhremenko-vladimir <vladimir.okhremenko@gmail.com>
2022-08-20 21:01:57 +0700

    add new models

--- good_job/models.py
+++ good_job/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
 
 
 class Company(models.Model):
     company_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=32)
     location = models.CharField(max_length=32)
-    logo = models.ImageField(upload_to='company_logo/', default='https://place-hold.it/100x60')
+    logo = models.ImageField(upload_to='logo/company/', default='https://place-hold.it/100x60')
     description = models.TextField()
     employee_count = models.IntegerField()
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def amount_vacancies(self):
         return len(Vacancy.objects.filter(company__company_id=self.company_id))
@@ -16,7 +19,7 @@ class Company(models.Model):
 class Specialty(models.Model):
     code = models.CharField(max_length=32)
     title = models.CharField(max_length=32)
-    picture = models.ImageField(upload_to='specialty_logo/', default='https://place-hold.it/100x60')
+    picture = models.ImageField(upload_to='logo/specialty/', default='https://place-hold.it/100x60')
 
     def amount_vacancies(self):
         return len(Vacancy.objects.filter(specialty__code=self.code))
@@ -35,3 +38,11 @@ class Vacancy(models.Model):
 
     def skills_through_point(self):
         return ' • '.join(self.skills.split(', '))
+
+
+class Application(models.Model):
+    written_username = models.CharField(max_length=64)
+    written_phone = PhoneNumberField()
+    written_cover_letter = models.TextField()
+    vacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, related_name="applications")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="applications")
